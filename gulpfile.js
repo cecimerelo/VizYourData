b908c492243d56aa333bab0306c2569c491f3029
cecimerelo <ceci.merelo@gmail.com>
2020-10-20 19:55:08 +0200

    Configura grunt, closes #16

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const jest = require('gulp-jest').default;
-const gulp = require("gulp");
-
-gulp.task('unit', () => {
-    return gulp.src('tests/unit')
-        .pipe(jest({coverage: true}))
-})
-
-gulp.task('test', gulp.parallel('unit'));
