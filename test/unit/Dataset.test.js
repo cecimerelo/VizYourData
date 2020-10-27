9d6b8896d78b27f8c22ba6d7edf3687a54af6bf4
cecimerelo <ceci.merelo@gmail.com>
2020-10-27 10:43:26 +0100

    Resolve paths

--- test/unit/Dataset.test.js
+++ test/unit/Dataset.test.js
@@ -1,10 +1,9 @@
 import {describe, test} from "@jest/globals";
 import Dataset from "../../src/modules/Plots/Dataset";
+import { resolve } from "path";
 
-const homedir = require('os').homedir();
-
-const CSV_FILE_PATH = `${homedir}/test/files/2_TwoNum.csv`;
-const JSON_FILE_PATH = 'test/files/colors.json';
+const CSV_FILE_PATH = resolve("test/files/2_TwoNum.csv");
+const JSON_FILE_PATH = resolve("test/files/colors.json");
 
 describe ('Dataset', () => {
 
