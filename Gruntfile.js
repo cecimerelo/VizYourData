3666d37ce8261448fb16dec797ef0f58efee11d2
cecimerelo <ceci.merelo@gmail.com>
2020-11-10 19:52:26 +0100

    Eliminar docker de travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,21 +9,9 @@ module.exports = function (grunt) {
                     'test',
                     '--silent'
                 ]
-            },
-            travisTest: {
-                cmd: 'docker run -t -v <%=TRAVIS_BUILD_DIR%>:/test cecimerelo/vizyourdata'
             }
         }
     });
     grunt.loadNpmTasks('grunt-run');
-    grunt.loadNpmTasks('grunt-env');
-
     grunt.registerTask('test', ['run:test']);
-
-    grunt.registerTask('loadBuildDir', 'Load constants', function () {
-        grunt.config('TRAVIS_BUILD_DIR', process.env.TRAVIS_BUILD_DIR);
-        grunt.config('PATH', process.env.PATH);
-    });
-
-    grunt.registerTask('travisTest', ['loadBuildDir', 'run:travisTest']);
-}
+};
