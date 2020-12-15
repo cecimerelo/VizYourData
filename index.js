5586f7da4e94f853de8af2387eb36b485751cb80
cecimerelo <ceci.merelo@gmail.com>
2020-12-15 22:09:14 +0100

    Configure etcd

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const logger = require('./plugins/logger');
 const server = require('./api/routes.js');
+const {Etcd3} = require('etcd3');
 
-const message = 'Server listening at ' + 8080;
-server.listen(8080, function() {
-    logger.log('INFO', message);
-});
+const CONFIG_URL = process.env.CONFIG_URL || 'http://localhost:8080';
+
+let port;
+
+const run = async (configUrl) => {
+    const client = new Etcd3({
+        hosts: configUrl
+    })
+
+    port = await client.get("API_PORT").string();
+    port = port || process.env.API_PORT || 8080;
+
+    const message = 'Server listening at ' + port;
+    server.listen(port, function () {
+        logger.log('INFO', message);
+    });
+};
+
+run(CONFIG_URL);
