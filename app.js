f52947c0f4c7a6cc9b4823288248d25f4b572085
cecimerelo <ceci.merelo@gmail.com>
2020-12-16 18:54:12 +0100

    Make Etcd great again

--- app.js
+++ app.js
@@ -3,27 +3,31 @@ const logger = require('./plugins/logger');
 const {Etcd3} = require('etcd3');
 const routes = require('./api/routes');
 
-const ETCD_HOST = process.env.ETCD_IP;
+const ETCD_HOST = process.env.ETCD_HOST;
 const ETCD_PORT = process.env.ETCD_PORT;
 const CONFIG_URL = `${ETCD_HOST}:${ETCD_PORT}` || 'http://localhost:8080';
 
+const client = new Etcd3({
+    hosts: CONFIG_URL
+})
+
 const server = restify.createServer();
-server.use(function (req,res,next) {
+server.use(function (req, res, next) {
     const request_method = req.method
     const request_url = req.url
-    logger.log('INFO', `${request_method} ${request_url}` )
+    logger.log('INFO', `${request_method} ${request_url}`)
     next()
 });
 
 let port;
+let root;
 
-const run = async (configUrl) => {
-    const client = new Etcd3({
-        hosts: configUrl
-    })
+const run = async () => {
+    root = await client.get("ROOT").string();
+    port = await client.get("PORT").string();
 
-    port = await client.get("API_PORT").string();
-    port = port || process.env.API_PORT || 8080;
+    root = root || process.env.API_ROOT || "";
+    port = port || process.env.API_PORT || 8000;
 
     const message = 'Server listening at ' + port;
     server.listen(port, function () {
@@ -31,8 +35,6 @@ const run = async (configUrl) => {
     });
 };
 
-run(CONFIG_URL);
-
+run();
 routes(server);
-
 module.exports = server;
