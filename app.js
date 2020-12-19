f3e7b1562d46043a1cb22357359973e09ff5deef
cecimerelo <ceci.merelo@gmail.com>
2020-12-19 18:03:11 +0100

    Fixed npm run dev

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const restify = require("restify");
 const logger = require('./plugins/logger');
-const {Etcd3} = require('etcd3');
 const routes = require('./api/routes');
-
-const ETCD_HOST = process.env.ETCD_HOST;
-const ETCD_PORT = process.env.ETCD_PORT;
-const CONFIG_URL = `${ETCD_HOST}:${ETCD_PORT}` || 'http://localhost:8080';
-
-const client = new Etcd3({
-    hosts: CONFIG_URL
-})
+const config = require('./config')
 
 const server = restify.createServer();
 server.use(function (req, res, next) {
@@ -19,22 +11,11 @@ server.use(function (req, res, next) {
     next()
 });
 
-let port;
-let root;
+const port = config.PORT;
+const message = 'Server listening at ' + port;
+server.listen(port, function () {
+    logger.log('INFO', message);
+});
 
-const run = async () => {
-    root = await client.get("ROOT").string();
-    port = await client.get("PORT").string();
-
-    root = root || process.env.API_ROOT || "";
-    port = port || process.env.API_PORT || 8000;
-
-    const message = 'Server listening at ' + port;
-    server.listen(port, function () {
-        logger.log('INFO', message);
-    });
-};
-
-run();
 routes(server);
 module.exports = server;
