bd65070ccce1415e6c6329abc89ad4d6e9189ca6
cecimerelo <ceci.merelo@gmail.com>
2021-01-10 22:35:18 +0100

    Dynamic host

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
+const os = require('os')
 const restify = require('restify')
 const consola = require('consola')
 const routes = require('./api/routes')
 const middleware = require('./middleware')
 
 const app = restify.createServer()
-
-const { host, port } = { host: 'localhost', port: 8080 }
+const { host, port } = { host: os.hostname(), port: 8080 }
 
 // Set variables Host y port en etcd.
 const serverConfigKeys = { PORT: port }
@@ -15,7 +15,7 @@ const serverConfigKeysJson = JSON.stringify(serverConfigKeys)
 fs.writeFileSync('./static/server_config_keys.json', serverConfigKeysJson, 'utf8')
 
 // Hacemos que el servidor escuche el el puerto especificado, para poder hacer las solicitudes
-app.listen(port)
+app.listen(port, host)
 
 // configuramos el middleware
 middleware(app)
