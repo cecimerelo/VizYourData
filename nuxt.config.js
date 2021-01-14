cb8bf1999d1ef9a2907174a6bccc3a5a61b34393
cecimerelo <ceci.merelo@gmail.com>
2021-01-14 23:11:41 +0100

    Changed host to localhost value

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,6 @@ const config = {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
-  server: {
-    port: 3000, // default: 3000
-    host: '0.0.0.0' // default: localhost
-  },
-
   serverMiddleware: [
     '~/server/middleware',
     {
